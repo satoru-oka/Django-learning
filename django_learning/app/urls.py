d3ba925558cef08a12cdeb71fe07195bacbb6adf
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-05-21 18:07:04 +0900

    ML-196 other

--- django_learning/app/urls.py
+++ django_learning/app/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     path('hello', views.index, name='index'),
     path('page/<str:user_name>', views.user_page, name='user_name'),
     path('number_page/<str:user_name>/<int:number>', views.number_page, name='number_page'),
-
+    path('add/<int:num1>/<int:num2>', views.add, name='add'),
+    path('subtraction/<str:num1>/<str:num2>', views.subtraction, name='subtraction'),
+    path('division/<str:num1>/<str:num2>', views.division, name='division'),
+    
 ]
--- django_learning/app/views.py
+++ django_learning/app/views.py
@@ -11,3 +11,19 @@ def user_page(request, user_name):
 def number_page(request, user_name, number):
     user_name = user_name.upper()
     return HttpResponse(f'<h1>page number = {user_name}\'s {number}</h1>')
+
+# num add
+def add(request, num1, num2):
+    return HttpResponse(f'<h1>{num1 + num2}</h1>')
+
+# num subtraciton
+def subtraction(request, num1, num2):
+    num1 = float(num1)
+    num2 = float(num2)
+    return HttpResponse(f'<h1>{num1 - num2}</h1>')
+
+# num division
+def division(request, num1, num2):
+    num1 = float(num1)
+    num2 = float(num2)
+    return HttpResponse(f'<h1>{round(num1/num2)}</h1>')
