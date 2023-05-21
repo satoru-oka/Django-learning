1bf064d64b8c51606c8c121efef702309cd43d07
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2023-05-21 17:44:12 +0900

    ML-196 URL dispatch

--- django_learning/app/urls.py
+++ django_learning/app/urls.py
@@ -6,4 +6,7 @@ app_name = 'app'
 
 urlpatterns = [
     path('hello', views.index, name='index'),
+    path('page/<str:user_name>', views.user_page, name='user_name'),
+    path('number_page/<str:user_name>/<int:number>', views.number_page, name='number_page'),
+
 ]
--- django_learning/app/views.py
+++ django_learning/app/views.py
@@ -4,3 +4,10 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('<h1>Hello</h1>')
+
+def user_page(request, user_name):
+    return HttpResponse(f'<h1>{user_name}\'s page</h1>')
+
+def number_page(request, user_name, number):
+    user_name = user_name.upper()
+    return HttpResponse(f'<h1>page number = {user_name}\'s {number}</h1>')
